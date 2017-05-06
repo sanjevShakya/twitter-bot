bb00c63b320fea8bf51649a69c8d258e5f18995f
Sanjeev Shakya <sanjevsakya@gmail.com>
2017-05-06 22:15:16 +0545

    sample tweet post example

--- index.js
+++ index.js
@@ -7,3 +7,13 @@ var bot = new Twit({
   access_token_secret:  process.env.LEARNING_BOT_ACCESS_TOKEN_SECRET,
   timeout_ms: 60 * 1000
 });
+
+bot.post('statuses/update', {
+  status: "Hello from bot!"
+}, function(err, data, response){
+  if(err) {
+    console.log(err);
+  } else {
+    console.log(data.text + "was tweeted");
+  }
+})
